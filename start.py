b26e4a030fd2d7b8096037d2e5f7e396d710ae13
phinehasz <zhhiyp@qq.com>
2018-12-20 22:58:14 +0800

    command start

---
+++ start.py
@@ -0,0 +1,9 @@
+# coding=UTF-8
+from lib import url_parser, download
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1:][0]
+    if 'https://v.qq.com/x/page/' in url:
+        download_url, video_name = url_parser.tecent_parse(url)
+    download.down(download_url, video_name)
