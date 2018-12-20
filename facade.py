0e1ca51db46ea5e196e8862cb0484a586afacaf5
phinehasz <zhhiyp@qq.com>
2018-12-20 22:18:03 +0800

    if main

--- facade.py
+++ facade.py
@@ -1,6 +1,7 @@
 # coding=UTF-8
 from lib import url_parser, download
 
-url = 'https://v.qq.com/x/page/i0145lvzyj0.html'
-download_url, video_name = url_parser.tecent_parse(url)
-download.down(download_url, video_name)
+if __name__ == '__main__':
+    url = 'https://v.qq.com/x/page/i0145lvzyj0.html'
+    download_url, video_name = url_parser.tecent_parse(url)
+    download.down(download_url, video_name)
